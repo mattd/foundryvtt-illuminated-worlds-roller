785f77b8be824b3e7c4e72dca682527fc86e58e9
Matt Dawson <matthewtdawson@gmail.com>
2023-08-14 23:29:08 -0400

    Hook prettier back up

--- module/scripts/init.mjs
+++ module/scripts/init.mjs
@@ -1,4 +1,4 @@
-import { IlluminatedWorldsRoller } from './illuminated-worlds-roller.mjs';
+import { IlluminatedWorldsRoller } from "./illuminated-worlds-roller.mjs";
 
 Hooks.once("ready", () => {
     game.illuminatedWorldsRoller = new IlluminatedWorldsRoller();
@@ -20,131 +20,109 @@ Hooks.on("renderSceneControls", (app, html) => {
 
 Hooks.once("setup", () => {
     const defaultActions = [
-        'Move',
-        'Strike',
-        'Control',
-        'Sway',
-        'Read',
-        'Hide',
-        'Survey',
-        'Focus',
-        'Sense'
+        "Move",
+        "Strike",
+        "Control",
+        "Sway",
+        "Read",
+        "Hide",
+        "Survey",
+        "Focus",
+        "Sense"
     ];
 
-    game.settings.register(
-        "illuminated-worlds-roller", "backgroundColor",
-        {
-            "name": game.i18n.localize(
-                "IlluminatedWorldsRoller.backgroundColorName"
+    game.settings.register("illuminated-worlds-roller", "backgroundColor", {
+        name: game.i18n.localize("IlluminatedWorldsRoller.backgroundColorName"),
+        hint: game.i18n.localize("IlluminatedWorldsRoller.backgroundColorHint"),
+        scope: "world",
+        config: true,
+        choices: {
+            gray: game.i18n.localize(
+                "IlluminatedWorldsRoller.backgroundColorGray"
             ),
-            "hint": game.i18n.localize(
-                "IlluminatedWorldsRoller.backgroundColorHint"
-            ),
-            "scope": "world",
-            "config": true,
-            "choices": {
-                "gray": game.i18n.localize(
-                    "IlluminatedWorldsRoller.backgroundColorGray"
-                ),
-                "black": game.i18n.localize(
-                    "IlluminatedWorldsRoller.backgroundColorBlack"
-                )
-            },
-            "default": "gray",
-            "type": String
-        }
-    );
+            black: game.i18n.localize(
+                "IlluminatedWorldsRoller.backgroundColorBlack"
+            )
+        },
+        default: "gray",
+        type: String
+    });
 
     game.settings.register(
-        "illuminated-worlds-roller", "maxStandardDiceCount",
+        "illuminated-worlds-roller",
+        "maxStandardDiceCount",
         {
-            "name": game.i18n.localize(
+            name: game.i18n.localize(
                 "IlluminatedWorldsRoller.MaxStandardDiceCountName"
             ),
-            "hint": game.i18n.localize(
+            hint: game.i18n.localize(
                 "IlluminatedWorldsRoller.MaxStandardDiceCountHint"
             ),
-            "scope": "world",
-            "config": true,
-            "default": 10,
-            "type": Number
+            scope: "world",
+            config: true,
+            default: 10,
+            type: Number
         }
     );
 
     game.settings.register(
-        "illuminated-worlds-roller", "defaultStandardDiceCount",
+        "illuminated-worlds-roller",
+        "defaultStandardDiceCount",
         {
-            "name": game.i18n.localize(
+            name: game.i18n.localize(
                 "IlluminatedWorldsRoller.DefaultStandardDiceCountName"
             ),
-            "hint": game.i18n.localize(
+            hint: game.i18n.localize(
                 "IlluminatedWorldsRoller.DefaultStandardDiceCountHint"
             ),
-            "scope": "world",
-            "config": true,
-            "default": 2,
-            "type": Number
+            scope: "world",
+            config: true,
+            default: 2,
+            type: Number
         }
     );
 
-    game.settings.register(
-        "illuminated-worlds-roller", "actions",
-        {
-            "name": game.i18n.localize("IlluminatedWorldsRoller.ActionsName"),
-            "hint": game.i18n.localize("IlluminatedWorldsRoller.ActionsHint"),
-            "scope": "world",
-            "config": true,
-            "type": String,
-            "default": defaultActions.map(item => {
+    game.settings.register("illuminated-worlds-roller", "actions", {
+        name: game.i18n.localize("IlluminatedWorldsRoller.ActionsName"),
+        hint: game.i18n.localize("IlluminatedWorldsRoller.ActionsHint"),
+        scope: "world",
+        config: true,
+        type: String,
+        default: defaultActions
+            .map(item => {
                 return game.i18n.localize(
                     `IlluminatedWorldsRoller.DefaultAction${item}`
-                )
-            }).join(',')
-        }
-    );
+                );
+            })
+            .join(",")
+    });
 
-    game.settings.register(
-        "illuminated-worlds-roller", "defaultStakes",
-        {
-            "name": game.i18n.localize(
-                "IlluminatedWorldsRoller.DefaultStakesName"
-            ),
-            "hint": game.i18n.localize(
-                "IlluminatedWorldsRoller.DefaultStakesHint"
-            ),
-            "scope": "world",
-            "config": true,
-            "type": String,
-            "choices": {
-                "high": game.i18n.localize(
-                    "IlluminatedWorldsRoller.StakesHigh"
-                ),
-                "normal": game.i18n.localize(
-                    "IlluminatedWorldsRoller.StakesNormal"
-                ),
-                "low": game.i18n.localize(
-                    "IlluminatedWorldsRoller.StakesLow"
-                )
-            },
-            "default": "normal"
-        }
-    );
+    game.settings.register("illuminated-worlds-roller", "defaultStakes", {
+        name: game.i18n.localize("IlluminatedWorldsRoller.DefaultStakesName"),
+        hint: game.i18n.localize("IlluminatedWorldsRoller.DefaultStakesHint"),
+        scope: "world",
+        config: true,
+        type: String,
+        choices: {
+            high: game.i18n.localize("IlluminatedWorldsRoller.StakesHigh"),
+            normal: game.i18n.localize("IlluminatedWorldsRoller.StakesNormal"),
+            low: game.i18n.localize("IlluminatedWorldsRoller.StakesLow")
+        },
+        default: "normal"
+    });
 
-    game.settings.register(
-        "illuminated-worlds-roller", "gildedAppearance",
-        {
-            "name": game.i18n.localize(
-                "IlluminatedWorldsRoller.GildedAppearanceName"
-            ),
-            "hint": game.i18n.localize(
-                "IlluminatedWorldsRoller.GildedAppearanceHint"
-            ),
-            "scope": "world",
-            "config": true,
-            "type": String,
-            "default": "radiant"
-        }
-    );
+    game.settings.register("illuminated-worlds-roller", "gildedAppearance", {
+        name: game.i18n.localize(
+            "IlluminatedWorldsRoller.GildedAppearanceName"
+        ),
+        hint: game.i18n.localize(
+            "IlluminatedWorldsRoller.GildedAppearanceHint"
+        ),
+        scope: "world",
+        config: true,
+        type: String,
+        default: "radiant"
+    });
 });
 
 console.log("IlluminatedWorldsRoller | Illuminated Worlds Roller loaded");
